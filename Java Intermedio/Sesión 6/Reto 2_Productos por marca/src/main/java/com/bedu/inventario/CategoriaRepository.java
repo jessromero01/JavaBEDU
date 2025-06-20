b6d8acf296cb61d6c27b1fe16be131a5d821c3e9
Jessica Romero <jessicaromeropa@gmail.com>
2025-06-19 19:30:03 -0600

    Delete Java Intermedio directory

--- Java Intermedio/Sesión 6/Reto 2_Productos por marca/src/main/java/com/bedu/inventario/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bedu.inventario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- Java Intermedio/Sesión 6/Reto 2_Productos por marca/src/main/java/com/bedu/inventario/MarcaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bedu.inventario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarcaRepository extends JpaRepository<Marca, Long>
-{
-}
